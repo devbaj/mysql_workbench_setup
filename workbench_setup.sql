deb514b38114b336b2e9725ddfe179db29e6f576
Brett Jory <bjory7@gmail.com>
2019-03-26 16:25:52 -0700

    all assignment reqs completed

--- workbench_setup.sql
+++ workbench_setup.sql
@@ -1,1 +1,26 @@
-USE twitter
+USE twitter;
+
+SELECT 
+    tweet
+FROM
+    tweets
+WHERE
+    created_at < '2009-01-01 00:00:00';
+
+INSERT INTO users (first_name, last_name, handle, birthday)
+VALUES ('Brett', 'Jory', 'Kras', '1993-02-09');
+
+SELECT 
+    *
+FROM
+    users;
+    
+UPDATE users 
+SET 
+    handle = 'bjory'
+WHERE
+    id = 6;
+
+DELETE FROM users 
+WHERE
+    id = 6;
